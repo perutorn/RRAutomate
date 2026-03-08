66b08b530151bc93c48fe1fd194b507beaff21f7
m.ciachowski <121774245+perutorn@users.noreply.github.com>
2026-03-08 08:09:34 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'RRAutomate-2.0';
+// Ponieważ wszystko jest w index.html, potrzebujesz tylko jego i manifestu
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon192.png',
+    './icon512.png'
+];
+
+// 1. Instalacja
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('SW: Buforowanie monolitu index.html');
+            return cache.addAll(ASSETS);
+        })
+    );
+    self.skipWaiting(); // Wymusza aktywację nowej wersji od razu
+});
+
+// 2. Aktywacja
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// 3. Obsługa zapytań
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Zwróć z cache, a jeśli nie ma (np. nowe ikony), pobierz z sieci
+            return response || fetch(event.request);
+        })
+    );
+});
